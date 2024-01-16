98cd5f650ba6f260c929f6aeef20564b49e6d8f1
test1883 <parth.eng1210@gmail.com>
2024-01-16 10:27:08 +0000

    feat: added main contract

---
+++ contracts/contracts/ChainGHO.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {TestnetERC20} from "./test/TestnetERC20.sol";
+import {IPool} from "node_modules/@aave/core-v3/contracts/interfaces/IPool.sol";
+import {GhoToken} from "contracts/mocks/gho/GhoToken.sol";
+import {IGhoToken} from "contracts/mocks/gho/interfaces/IGhoToken.sol";
+
+contract ChainGHO {
+
+}
