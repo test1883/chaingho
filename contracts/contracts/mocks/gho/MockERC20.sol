7d135891255ca5463e6c0022b71550fea8fd8855
test1883 <parth.eng1210@gmail.com>
2024-01-16 10:45:15 +0000

    refactor: separated mocks

---
+++ contracts/contracts/mocks/gho/MockERC20.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT-only
+pragma solidity ^0.8.0;
+
+import {ERC20} from "../../gho/ERC20.sol";
+
+contract MockERC20 is ERC20 {}
---
+++ contracts/contracts/mocks/gho/MockGhoToken.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT-only
+pragma solidity ^0.8.0;
+
+import {GhoToken} from "../../gho/GhoToken.sol";
+
+contract MockGhoToken is GhoToken {}
