b1f192eabc5e702d5ad63db4b2ccd0783cde8446
test1883 <parth.eng1210@gmail.com>
2024-01-18 06:01:12 +0000

    refactor: moved aave contracts

--- contracts/contracts/ChainGHO.sol
+++ contracts/contracts/ChainGHO.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {TestnetERC20} from "./test/TestnetERC20.sol";
+import {IERC20WithPermit} from 'node_modules/@aave/core-v3/contracts/interfaces/IERC20WithPermit.sol';
 import {IPool} from "node_modules/@aave/core-v3/contracts/interfaces/IPool.sol";
-import {GhoToken} from "contracts/mocks/gho/GhoToken.sol";
-import {IGhoToken} from "contracts/mocks/gho/interfaces/IGhoToken.sol";
+import {GhoToken} from "contracts/gho/GhoToken.sol";
+import {IGhoToken} from "contracts/gho/interfaces/IGhoToken.sol";
 
 contract ChainGHO {
+    IERC20WithPermit[] Erc20Pools;
+    IPool Pool;
+    GhoToken Gho;
 
+    /**
+   * @dev Function to mint tokens
+   * @param erc20pools Contract Addresses of ERC20 - DAI | LINK | USDC | WBTC | WETH | USDT | AAVE | EURS pools
+   * @param pool Aave pool address
+   * @param gho Gho Reserve address
+   * @return A boolean that indicates if the operation was successful.
+   */
+
+    constructor (IERC20WithPermit[] erc20pools, IPool pool, GhoToken gho) {
+        Erc20Pools = erc20pools;
+        Pool = pool;
+        Gho = gho;
+    }
 }
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+require("dotenv").config()
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+      chainId: 31337
+    },
+    polygonMumbai: {
+      url: "https://polygon-mainnet.infura.io/v3/" + process.env.infura,
+      chainId: 80001,
+      accounts: [process.env.key!]
+    }
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts'
+  },
 };
 
 export default config;
